7e4bd681bab6b56cb71e3b37bfc01d2d273f10d0
Burak <burakturk@gmail.com>
2023-11-10 07:15:11 +0300

    images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+import SearchHeader from "./SearchHeader";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <SearchHeader/>
+  </div>;
 }
 
 export default App;
--- src/SearchHeader.js
+++ src/SearchHeader.js
@@ -1,1 +1,9 @@
-ffc
+function SearchHeader() {
+  return <div className="App">
+  <form>
+    <label>Ne arıyorsunuz?</label>
+  </form>
+  </div>;
+}
+
+export default SearchHeader;
