144aeeb66b8007df3e97b9c59886cd4240b6c0a2
Burak <burakturk@gmail.com>
2023-11-12 11:39:58 +0300

    unSplash

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
 import SearchHeader from "./SearchHeader";
+import axios from "axios";
 
 function App() {
-  return <div className="App">
-    <SearchHeader/>
-  </div>;
+  const searchImages = (term) => {
+    axios.get("https://api.unsplash.com/search/photos", {
+      headers: {
+        Authorization: "Client-ID toJ37pAx-VwsXof8T_LTwxJe6Xu9xTmshD4SQN62Gas",
+      },
+    });
+  };
+  const handleSubmit = (term) => {
+    debugger;
+    console.log(term);
+    searchImages(term);
+  };
+  return (
+    <div className="App">
+      <SearchHeader onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
 export default App;
