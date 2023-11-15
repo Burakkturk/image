5585eeff526618c471f605c951ad2165df064c70
Burak <burakturk@gmail.com>
2023-11-15 14:51:06 +0300

    Search

--- src/SearchHeader.js
+++ src/SearchHeader.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
+
 function SearchHeader({ search }) {
+  const [valueInput, setValue] = useState("");
   const handleFormSubmit = (event) => {
     event.preventDefault();
     debugger;
     search("can");
   };
 
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <div className="searchDiv">
       <form onSubmit={handleFormSubmit}>
         <label>Ne Arıyorsunuz?</label>
-        <input />
+        <input value={valueInput} onChange={handleChange} />
+        <div>{valueInput}</div>
       </form>
     </div>
   );
